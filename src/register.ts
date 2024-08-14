c27b1939b77a13eaf31f7285a08a72d28842c8e0
saturnaliam <luciacdev@gmail.com>
2024-08-13 20:55:05 -0500

    error handling inside of command registering

--- src/register.ts
+++ src/register.ts
@@ -1,18 +1,19 @@
 import { CommandInteraction, REST, Routes } from "npm:discord.js";
 import ping from "./commands/ping.ts";
+import { Logger } from "./logger.ts";
 
 const commands = [
     ping
 ]
 
-export async function register() {
+export function register() {
     const rest = new REST({ version: '10' }).setToken(Deno.env.get("TOKEN") || "");
 
-    try {
-        await rest.put(Routes.applicationCommands("1272283592831012936"), { body: commands.map((command) => command.command) });
-    } catch(error) {
-        console.error(error);
-    }
+    rest.put(Routes.applicationCommands(Deno.env.get("APPLICATION_ID") || "0"), { body: commands.map((command) => command.command) })
+        .catch((error) => {
+            Logger.error("REGISTER", error);
+            Deno.exit(1);
+        });
 }
 
 export function handle(interaction: CommandInteraction) {
